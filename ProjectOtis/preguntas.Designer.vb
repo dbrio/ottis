fbaa8d613e50e5252e767881c4c2333670bf3753
dbrio2011 <dbrio2011@gmail.com>
2014-03-25 02:59:30 +0000

    pregunta

--- ProjectOtis/preguntas.Designer.vb
+++ ProjectOtis/preguntas.Designer.vb
@@ -22,16 +22,119 @@ Partial Class preguntas
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.PreguntasLabel = New System.Windows.Forms.Label()
+        Me.Preguntas1BindingSource3 = New System.Windows.Forms.BindingSource(Me.components)
+        Me.DatasetOtis = New ProjectOtis.DatasetOtis()
+        Me.PreguntasBindingSource = New System.Windows.Forms.BindingSource(Me.components)
+        Me.PreguntasTableAdapter = New ProjectOtis.DatasetOtisTableAdapters.PreguntasTableAdapter()
+        Me.TableAdapterManager = New ProjectOtis.DatasetOtisTableAdapters.TableAdapterManager()
+        Me.Preguntas1BindingSource = New System.Windows.Forms.BindingSource(Me.components)
+        Me.Preguntas1BindingSource1 = New System.Windows.Forms.BindingSource(Me.components)
+        Me.Preguntas1BindingSource2 = New System.Windows.Forms.BindingSource(Me.components)
+        Me.Preguntas1TableAdapter = New ProjectOtis.DatasetOtisTableAdapters.Preguntas1TableAdapter()
+        Me.LabelPregunta = New System.Windows.Forms.Label()
+        CType(Me.Preguntas1BindingSource3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.DatasetOtis, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PreguntasBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Preguntas1BindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Preguntas1BindingSource1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Preguntas1BindingSource2, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
+        '
+        'PreguntasLabel
+        '
+        Me.PreguntasLabel.DataBindings.Add(New System.Windows.Forms.Binding("Text", Me.Preguntas1BindingSource3, "Preguntas", True))
+        Me.PreguntasLabel.Location = New System.Drawing.Point(380, 122)
+        Me.PreguntasLabel.Name = "PreguntasLabel"
+        Me.PreguntasLabel.Size = New System.Drawing.Size(406, 23)
+        Me.PreguntasLabel.TabIndex = 2
+        Me.PreguntasLabel.Text = "Label1"
+        '
+        'Preguntas1BindingSource3
+        '
+        Me.Preguntas1BindingSource3.DataMember = "Preguntas1"
+        Me.Preguntas1BindingSource3.DataSource = Me.DatasetOtis
+        '
+        'DatasetOtis
+        '
+        Me.DatasetOtis.DataSetName = "DatasetOtis"
+        Me.DatasetOtis.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
+        '
+        'PreguntasBindingSource
+        '
+        Me.PreguntasBindingSource.DataMember = "Preguntas"
+        Me.PreguntasBindingSource.DataSource = Me.DatasetOtis
+        '
+        'PreguntasTableAdapter
+        '
+        Me.PreguntasTableAdapter.ClearBeforeFill = True
+        '
+        'TableAdapterManager
+        '
+        Me.TableAdapterManager.AlumnosTableAdapter = Nothing
+        Me.TableAdapterManager.BackupDataSetBeforeUpdate = False
+        Me.TableAdapterManager.CategoríaTableAdapter = Nothing
+        Me.TableAdapterManager.Preguntas1TableAdapter = Nothing
+        Me.TableAdapterManager.PreguntasTableAdapter = Me.PreguntasTableAdapter
+        Me.TableAdapterManager.UpdateOrder = ProjectOtis.DatasetOtisTableAdapters.TableAdapterManager.UpdateOrderOption.InsertUpdateDelete
+        '
+        'Preguntas1BindingSource
+        '
+        Me.Preguntas1BindingSource.DataSource = Me.DatasetOtis
+        Me.Preguntas1BindingSource.Position = 0
+        '
+        'Preguntas1BindingSource1
+        '
+        Me.Preguntas1BindingSource1.DataSource = Me.DatasetOtis
+        Me.Preguntas1BindingSource1.Position = 0
+        '
+        'Preguntas1BindingSource2
+        '
+        Me.Preguntas1BindingSource2.DataSource = Me.DatasetOtis
+        Me.Preguntas1BindingSource2.Position = 0
+        '
+        'Preguntas1TableAdapter
+        '
+        Me.Preguntas1TableAdapter.ClearBeforeFill = True
+        '
+        'LabelPregunta
+        '
+        Me.LabelPregunta.AutoSize = True
+        Me.LabelPregunta.Location = New System.Drawing.Point(12, 9)
+        Me.LabelPregunta.Name = "LabelPregunta"
+        Me.LabelPregunta.Size = New System.Drawing.Size(39, 13)
+        Me.LabelPregunta.TabIndex = 0
+        Me.LabelPregunta.Text = "Label1"
         '
         'preguntas
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(1133, 451)
+        Me.Controls.Add(Me.PreguntasLabel)
+        Me.Controls.Add(Me.LabelPregunta)
         Me.Name = "preguntas"
         Me.Text = "preguntas"
+        CType(Me.Preguntas1BindingSource3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.DatasetOtis, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PreguntasBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Preguntas1BindingSource, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Preguntas1BindingSource1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Preguntas1BindingSource2, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
+    Friend WithEvents DatasetOtis As ProjectOtis.DatasetOtis
+    Friend WithEvents PreguntasBindingSource As System.Windows.Forms.BindingSource
+    Friend WithEvents PreguntasTableAdapter As ProjectOtis.DatasetOtisTableAdapters.PreguntasTableAdapter
+    Friend WithEvents TableAdapterManager As ProjectOtis.DatasetOtisTableAdapters.TableAdapterManager
+    Friend WithEvents Preguntas1BindingSource As System.Windows.Forms.BindingSource
+    Friend WithEvents Preguntas1BindingSource1 As System.Windows.Forms.BindingSource
+    Friend WithEvents Preguntas1BindingSource2 As System.Windows.Forms.BindingSource
+    Friend WithEvents Preguntas1BindingSource3 As System.Windows.Forms.BindingSource
+    Friend WithEvents Preguntas1TableAdapter As ProjectOtis.DatasetOtisTableAdapters.Preguntas1TableAdapter
+    Friend WithEvents PreguntasLabel As System.Windows.Forms.Label
+    Friend WithEvents LabelPregunta As System.Windows.Forms.Label
 End Class
--- ProjectOtis/preguntas.vb
+++ ProjectOtis/preguntas.vb
@@ -1,3 +1,21 @@
 ﻿Public Class preguntas
 
+    Private Sub preguntas_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'TODO: esta línea de código carga datos en la tabla 'DatasetOtis.Preguntas1' Puede moverla o quitarla según sea necesario.
+        'Me.Preguntas1TableAdapter.Fill(Me.DatasetOtis.Preguntas1)
+        'TODO: esta línea de código carga datos en la tabla 'DatasetOtis.Preguntas' Puede moverla o quitarla según sea necesario.
+
+        Dim Pregunta As String = Preguntas1TableAdapter.Fill(Me.DatasetOtis.Preguntas1, 1)
+        LabelPregunta.Text = Pregunta
+
+    End Sub
+
+    'Private Sub PreguntasBindingNavigatorSaveItem_Click(sender As Object, e As EventArgs)
+    '    Me.Validate()
+    '    Me.PreguntasBindingSource.EndEdit()
+    '    Me.TableAdapterManager.UpdateAll(Me.DatasetOtis)
+
+    'End Sub
+
+    
 End Class
