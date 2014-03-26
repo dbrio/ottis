1c0a94d92a108aa196aaffdbd56ea969e8e4b1b7
villalobosmario16 <villalobosmario16@gmail.com>
2014-03-26 03:29:09 +0000

    codigo del boton saltar y codigo para limpiar los radio button

--- ProjectOtis/preguntas.vb
+++ ProjectOtis/preguntas.vb
@@ -56,6 +56,12 @@
 
         idRespuesta = idRespuesta + 5
 
+        RespuestaRadioA.Checked = False
+        RadioButtonB.Checked = False
+        RadioButtonC.Checked = False
+        RadioButtonD.Checked = False
+        RadioButtonE.Checked = False
+
         If id = 81 Then
             ButtonOk.Enabled = False
         End If
@@ -65,5 +71,28 @@
 
     End Sub
 
-   
+    Private Sub btnSaltar_Click(sender As Object, e As EventArgs) Handles btnSaltar.Click
+        id = id + 1
+
+        Dim Pregunta As String = Preguntas1TableAdapter.Fill(Me.DatasetOtis.Preguntas1, id)
+        LabelPregunta.Text = Pregunta
+        Label1.Text = id
+
+        Dim Respueta1 As String = RespuestasTableAdapter.FillBy(idRespuesta, id)
+        RespuestaRadioA.Text = Respueta1
+        Dim Respueta2 As String = RespuestasTableAdapter.FillBy((idRespuesta + 1), id)
+        RadioButtonB.Text = Respueta2
+        Dim Respueta3 As String = RespuestasTableAdapter.FillBy((idRespuesta + 2), id)
+        RadioButtonC.Text = Respueta3
+        Dim Respueta4 As String = RespuestasTableAdapter.FillBy((idRespuesta + 3), id)
+        RadioButtonD.Text = Respueta4
+        Dim Respueta5 As String = RespuestasTableAdapter.FillBy((idRespuesta + 4), id)
+        RadioButtonE.Text = Respueta5
+
+        idRespuesta = idRespuesta + 5
+
+        If id = 81 Then
+            ButtonOk.Enabled = False
+        End If
+    End Sub
 End Class
