bcf4ec41c04e9aee67398d6b6091c28db0d622bb
villalobosmario16 <villalobosmario16@gmail.com>
2014-03-27 20:15:43 +0000

    diseno de formulario Ingresar Alumno.Designer.vb por cristian barahona

---
+++ ProjectOtis/Ingresar Alumno.Designer.vb
@@ -0,0 +1,238 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Ingresar_Alumno
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim RNPLabel As System.Windows.Forms.Label
+        Dim NombreLabel As System.Windows.Forms.Label
+        Dim ApellidosLabel As System.Windows.Forms.Label
+        Dim DireccionLabel As System.Windows.Forms.Label
+        Dim TelefonoLabel As System.Windows.Forms.Label
+        Dim CorreoLabel As System.Windows.Forms.Label
+        Me.DatasetOtis = New ProjectOtis.DatasetOtis()
+        Me.AlumnosBindingSource = New System.Windows.Forms.BindingSource(Me.components)
+        Me.AlumnosTableAdapter = New ProjectOtis.DatasetOtisTableAdapters.AlumnosTableAdapter()
+        Me.TableAdapterManager = New ProjectOtis.DatasetOtisTableAdapters.TableAdapterManager()
+        Me.RNPTextEdit = New DevExpress.XtraEditors.TextEdit()
+        Me.NombreTextEdit = New DevExpress.XtraEditors.TextEdit()
+        Me.ApellidosTextEdit = New DevExpress.XtraEditors.TextEdit()
+        Me.DireccionTextEdit = New DevExpress.XtraEditors.TextEdit()
+        Me.TelefonoTextEdit = New DevExpress.XtraEditors.TextEdit()
+        Me.CorreoTextEdit = New DevExpress.XtraEditors.TextEdit()
+        Me.ButtonIngresar = New System.Windows.Forms.Button()
+        RNPLabel = New System.Windows.Forms.Label()
+        NombreLabel = New System.Windows.Forms.Label()
+        ApellidosLabel = New System.Windows.Forms.Label()
+        DireccionLabel = New System.Windows.Forms.Label()
+        TelefonoLabel = New System.Windows.Forms.Label()
+        CorreoLabel = New System.Windows.Forms.Label()
+        CType(Me.DatasetOtis, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.AlumnosBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.RNPTextEdit.Properties, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.NombreTextEdit.Properties, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.ApellidosTextEdit.Properties, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.DireccionTextEdit.Properties, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.TelefonoTextEdit.Properties, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.CorreoTextEdit.Properties, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'DatasetOtis
+        '
+        Me.DatasetOtis.DataSetName = "DatasetOtis"
+        Me.DatasetOtis.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
+        '
+        'AlumnosBindingSource
+        '
+        Me.AlumnosBindingSource.DataMember = "Alumnos"
+        Me.AlumnosBindingSource.DataSource = Me.DatasetOtis
+        '
+        'AlumnosTableAdapter
+        '
+        Me.AlumnosTableAdapter.ClearBeforeFill = True
+        '
+        'TableAdapterManager
+        '
+        Me.TableAdapterManager.AlumnosTableAdapter = Me.AlumnosTableAdapter
+        Me.TableAdapterManager.BackupDataSetBeforeUpdate = False
+        Me.TableAdapterManager.CategoríaTableAdapter = Nothing
+        Me.TableAdapterManager.Preguntas1TableAdapter = Nothing
+        Me.TableAdapterManager.PreguntasTableAdapter = Nothing
+        Me.TableAdapterManager.RespuestasTableAdapter = Nothing
+        Me.TableAdapterManager.RespuestasUsuarioTableAdapter = Nothing
+        Me.TableAdapterManager.UpdateOrder = ProjectOtis.DatasetOtisTableAdapters.TableAdapterManager.UpdateOrderOption.InsertUpdateDelete
+        '
+        'RNPLabel
+        '
+        RNPLabel.AutoSize = True
+        RNPLabel.Location = New System.Drawing.Point(465, 46)
+        RNPLabel.Name = "RNPLabel"
+        RNPLabel.Size = New System.Drawing.Size(33, 13)
+        RNPLabel.TabIndex = 1
+        RNPLabel.Text = "RNP:"
+        '
+        'RNPTextEdit
+        '
+        Me.RNPTextEdit.DataBindings.Add(New System.Windows.Forms.Binding("EditValue", Me.AlumnosBindingSource, "RNP", True))
+        Me.RNPTextEdit.Location = New System.Drawing.Point(526, 43)
+        Me.RNPTextEdit.Name = "RNPTextEdit"
+        Me.RNPTextEdit.Size = New System.Drawing.Size(100, 20)
+        Me.RNPTextEdit.TabIndex = 2
+        '
+        'NombreLabel
+        '
+        NombreLabel.AutoSize = True
+        NombreLabel.Location = New System.Drawing.Point(42, 46)
+        NombreLabel.Name = "NombreLabel"
+        NombreLabel.Size = New System.Drawing.Size(47, 13)
+        NombreLabel.TabIndex = 3
+        NombreLabel.Text = "Nombre:"
+        '
+        'NombreTextEdit
+        '
+        Me.NombreTextEdit.DataBindings.Add(New System.Windows.Forms.Binding("EditValue", Me.AlumnosBindingSource, "Nombre", True))
+        Me.NombreTextEdit.Location = New System.Drawing.Point(103, 43)
+        Me.NombreTextEdit.Name = "NombreTextEdit"
+        Me.NombreTextEdit.Size = New System.Drawing.Size(100, 20)
+        Me.NombreTextEdit.TabIndex = 4
+        '
+        'ApellidosLabel
+        '
+        ApellidosLabel.AutoSize = True
+        ApellidosLabel.Location = New System.Drawing.Point(255, 46)
+        ApellidosLabel.Name = "ApellidosLabel"
+        ApellidosLabel.Size = New System.Drawing.Size(52, 13)
+        ApellidosLabel.TabIndex = 5
+        ApellidosLabel.Text = "Apellidos:"
+        '
+        'ApellidosTextEdit
+        '
+        Me.ApellidosTextEdit.DataBindings.Add(New System.Windows.Forms.Binding("EditValue", Me.AlumnosBindingSource, "Apellidos", True))
+        Me.ApellidosTextEdit.Location = New System.Drawing.Point(316, 43)
+        Me.ApellidosTextEdit.Name = "ApellidosTextEdit"
+        Me.ApellidosTextEdit.Size = New System.Drawing.Size(100, 20)
+        Me.ApellidosTextEdit.TabIndex = 6
+        '
+        'DireccionLabel
+        '
+        DireccionLabel.AutoSize = True
+        DireccionLabel.Location = New System.Drawing.Point(42, 105)
+        DireccionLabel.Name = "DireccionLabel"
+        DireccionLabel.Size = New System.Drawing.Size(55, 13)
+        DireccionLabel.TabIndex = 7
+        DireccionLabel.Text = "Direccion:"
+        '
+        'DireccionTextEdit
+        '
+        Me.DireccionTextEdit.DataBindings.Add(New System.Windows.Forms.Binding("EditValue", Me.AlumnosBindingSource, "Direccion", True))
+        Me.DireccionTextEdit.Location = New System.Drawing.Point(103, 102)
+        Me.DireccionTextEdit.Name = "DireccionTextEdit"
+        Me.DireccionTextEdit.Size = New System.Drawing.Size(100, 20)
+        Me.DireccionTextEdit.TabIndex = 8
+        '
+        'TelefonoLabel
+        '
+        TelefonoLabel.AutoSize = True
+        TelefonoLabel.Location = New System.Drawing.Point(255, 105)
+        TelefonoLabel.Name = "TelefonoLabel"
+        TelefonoLabel.Size = New System.Drawing.Size(52, 13)
+        TelefonoLabel.TabIndex = 9
+        TelefonoLabel.Text = "Telefono:"
+        '
+        'TelefonoTextEdit
+        '
+        Me.TelefonoTextEdit.DataBindings.Add(New System.Windows.Forms.Binding("EditValue", Me.AlumnosBindingSource, "Telefono", True))
+        Me.TelefonoTextEdit.Location = New System.Drawing.Point(316, 102)
+        Me.TelefonoTextEdit.Name = "TelefonoTextEdit"
+        Me.TelefonoTextEdit.Size = New System.Drawing.Size(100, 20)
+        Me.TelefonoTextEdit.TabIndex = 10
+        '
+        'CorreoLabel
+        '
+        CorreoLabel.AutoSize = True
+        CorreoLabel.Location = New System.Drawing.Point(465, 105)
+        CorreoLabel.Name = "CorreoLabel"
+        CorreoLabel.Size = New System.Drawing.Size(41, 13)
+        CorreoLabel.TabIndex = 11
+        CorreoLabel.Text = "Correo:"
+        '
+        'CorreoTextEdit
+        '
+        Me.CorreoTextEdit.DataBindings.Add(New System.Windows.Forms.Binding("EditValue", Me.AlumnosBindingSource, "Correo", True))
+        Me.CorreoTextEdit.Location = New System.Drawing.Point(526, 102)
+        Me.CorreoTextEdit.Name = "CorreoTextEdit"
+        Me.CorreoTextEdit.Size = New System.Drawing.Size(100, 20)
+        Me.CorreoTextEdit.TabIndex = 12
+        '
+        'ButtonIngresar
+        '
+        Me.ButtonIngresar.Location = New System.Drawing.Point(140, 222)
+        Me.ButtonIngresar.Name = "ButtonIngresar"
+        Me.ButtonIngresar.Size = New System.Drawing.Size(406, 67)
+        Me.ButtonIngresar.TabIndex = 13
+        Me.ButtonIngresar.Text = "Ingresar"
+        Me.ButtonIngresar.UseVisualStyleBackColor = True
+        '
+        'Ingresar_Alumno
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(701, 333)
+        Me.Controls.Add(Me.ButtonIngresar)
+        Me.Controls.Add(RNPLabel)
+        Me.Controls.Add(Me.RNPTextEdit)
+        Me.Controls.Add(NombreLabel)
+        Me.Controls.Add(Me.NombreTextEdit)
+        Me.Controls.Add(ApellidosLabel)
+        Me.Controls.Add(Me.ApellidosTextEdit)
+        Me.Controls.Add(DireccionLabel)
+        Me.Controls.Add(Me.DireccionTextEdit)
+        Me.Controls.Add(TelefonoLabel)
+        Me.Controls.Add(Me.TelefonoTextEdit)
+        Me.Controls.Add(CorreoLabel)
+        Me.Controls.Add(Me.CorreoTextEdit)
+        Me.Name = "Ingresar_Alumno"
+        Me.Text = "Ingresar_Alumno"
+        CType(Me.DatasetOtis, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.AlumnosBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.RNPTextEdit.Properties, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.NombreTextEdit.Properties, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.ApellidosTextEdit.Properties, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.DireccionTextEdit.Properties, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.TelefonoTextEdit.Properties, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.CorreoTextEdit.Properties, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents DatasetOtis As ProjectOtis.DatasetOtis
+    Friend WithEvents AlumnosBindingSource As System.Windows.Forms.BindingSource
+    Friend WithEvents AlumnosTableAdapter As ProjectOtis.DatasetOtisTableAdapters.AlumnosTableAdapter
+    Friend WithEvents TableAdapterManager As ProjectOtis.DatasetOtisTableAdapters.TableAdapterManager
+    Friend WithEvents RNPTextEdit As DevExpress.XtraEditors.TextEdit
+    Friend WithEvents NombreTextEdit As DevExpress.XtraEditors.TextEdit
+    Friend WithEvents ApellidosTextEdit As DevExpress.XtraEditors.TextEdit
+    Friend WithEvents DireccionTextEdit As DevExpress.XtraEditors.TextEdit
+    Friend WithEvents TelefonoTextEdit As DevExpress.XtraEditors.TextEdit
+    Friend WithEvents CorreoTextEdit As DevExpress.XtraEditors.TextEdit
+    Friend WithEvents ButtonIngresar As System.Windows.Forms.Button
+End Class
