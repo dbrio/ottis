7d7cbd6914ddc0266e79296d36579775fdf597c0
dbrio2011 <dbrio2011@gmail.com>
2014-03-27 20:53:56 +0000

    diseño

--- ProjectOtis/preguntas.vb
+++ ProjectOtis/preguntas.vb
@@ -11,6 +11,7 @@
         'TODO: esta línea de código carga datos en la tabla 'DatasetOtis.Preguntas1' Puede moverla o quitarla según sea necesario.
         'Me.Preguntas1TableAdapter.Fill(Me.DatasetOtis.Preguntas1)
         'TODO: esta línea de código carga datos en la tabla 'DatasetOtis.Preguntas' Puede moverla o quitarla según sea necesario.
+       
 
         Dim Pregunta As String = Preguntas1TableAdapter.Fill(Me.DatasetOtis.Preguntas1, 1)
         LabelPregunta.Text = Pregunta
