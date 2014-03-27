29a11f34bbead4dfbc9d0aa2b08ababd2d830029
dbrio2011@gmail.com <dbrio2011@gmail.com@75299b25-fe0f-f6b3-8b8a-2ee9f3d15202>
2014-03-27 20:10:20 +0000

    splashscreen

---
+++ trunk/ProjectOtis/splashscreen.vb
@@ -0,0 +1,10 @@
+﻿Public Class splashscreen
+
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
+
+    End Sub
+
+    Private Sub splashscreen_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
