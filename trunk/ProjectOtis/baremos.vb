c35f39638642b1396d70df28dd8f433e70ae9b93
dbrio2011@gmail.com <dbrio2011@gmail.com@75299b25-fe0f-f6b3-8b8a-2ee9f3d15202>
2014-03-30 05:28:35 +0000

    otros formularios login, listaUsuarios, baremos

---
+++ trunk/ProjectOtis/baremos.vb
@@ -0,0 +1,3 @@
+﻿Public Class baremos
+
+End Class
---
+++ trunk/ProjectOtis/login.Designer.vb
@@ -0,0 +1,100 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class login
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TextBoxUser = New System.Windows.Forms.TextBox()
+        Me.TextBoxPass = New System.Windows.Forms.TextBox()
+        Me.butonEntrar = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'TextBoxUser
+        '
+        Me.TextBoxUser.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBoxUser.Location = New System.Drawing.Point(170, 97)
+        Me.TextBoxUser.Name = "TextBoxUser"
+        Me.TextBoxUser.Size = New System.Drawing.Size(189, 31)
+        Me.TextBoxUser.TabIndex = 0
+        '
+        'TextBoxPass
+        '
+        Me.TextBoxPass.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBoxPass.Location = New System.Drawing.Point(170, 159)
+        Me.TextBoxPass.Name = "TextBoxPass"
+        Me.TextBoxPass.Size = New System.Drawing.Size(189, 31)
+        Me.TextBoxPass.TabIndex = 1
+        '
+        'butonEntrar
+        '
+        Me.butonEntrar.BackColor = System.Drawing.Color.Olive
+        Me.butonEntrar.ForeColor = System.Drawing.Color.White
+        Me.butonEntrar.Location = New System.Drawing.Point(170, 224)
+        Me.butonEntrar.Name = "butonEntrar"
+        Me.butonEntrar.Size = New System.Drawing.Size(189, 46)
+        Me.butonEntrar.TabIndex = 2
+        Me.butonEntrar.Text = "ENTRAR"
+        Me.butonEntrar.UseVisualStyleBackColor = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(67, 100)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(86, 25)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Usuario"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(41, 162)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(123, 25)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Contraseña"
+        '
+        'login
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.White
+        Me.ClientSize = New System.Drawing.Size(494, 371)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.butonEntrar)
+        Me.Controls.Add(Me.TextBoxPass)
+        Me.Controls.Add(Me.TextBoxUser)
+        Me.Name = "login"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents TextBoxUser As System.Windows.Forms.TextBox
+    Friend WithEvents TextBoxPass As System.Windows.Forms.TextBox
+    Friend WithEvents butonEntrar As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+End Class
